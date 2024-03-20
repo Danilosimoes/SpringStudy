0e997bb309b53a465fd49269852ab3649750035b
Danilosimoes <danilossimoes28@gmail.com>
2024-03-20 00:26:50 -0300

    initializer

---
+++ src/test/java/com/danilo/api/danilo/DaniloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danilo.api.danilo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaniloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
